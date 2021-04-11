a8935eef97abc64a1239bae7999750df4474aac4
Lee Roland <glroland@hotmail.com>
2021-04-11 10:39:35 -0400

    in flight yaml based integrations
    initial version of manual approach in spring boot

---
+++ manual-eda/src/main/java/com/glroland/myeda/manualeda/ManualEdaApplication.java
@@ -0,0 +1,13 @@
+package com.glroland.myeda.manualeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualEdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualEdaApplication.class, args);
+	}
+
+}
---
+++ manual-eda/src/test/java/com/glroland/myeda/manualeda/ManualEdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glroland.myeda.manualeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualEdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
